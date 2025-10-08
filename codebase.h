d37cfa18f75620a1c4b00410ce5f4a70302e874c
varshikageu <varshikasaini17@gmail.com>
2025-10-08 22:56:05 +0530

    Create codebase.h

---
+++ codebase.h
@@ -0,0 +1,20 @@
+#ifndef CODEBASE_H
+#define CODEBASE_H
+
+void findGcdLcm();
+void checkPrime();
+void findFactorial();
+void sumOfDigits();
+void checkArmstrong();
+void checkAnagram();
+void firstNonRepeatingChar();
+void removeDuplicates();
+void secondLargest();
+void findPairsWithSum();
+void checkBalancedParentheses();
+void reverseLinkedList();
+void findMiddleNode();
+void mergeSortedLists();
+void queueUsingStacks();
+
+#endif
